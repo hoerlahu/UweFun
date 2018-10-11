3d9b146a9e3247081ba2169dd885f259c09b53fb
uwe hoerber <uwe.hoerber@qupe.de>
2018-10-11 23:35:56 +0200

    initial firebase setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCWKCjrPUcl2xddkwck0xejb00zU1-BN78',
+    authDomain: 'test-9288d.firebaseapp.com',
+    databaseURL: 'https://test-9288d.firebaseio.com',
+    projectId: 'test-9288d',
+    storageBucket: 'test-9288d.appspot.com',
+    messagingSenderId: '842810745241'
+  }
 };
 
 /*
