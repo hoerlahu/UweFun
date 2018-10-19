37631c6180921fd197d95f9bcbc7d7bbb08abe54
uwe hoerber <uwe.hoerber@qupe.de>
2018-10-19 21:02:19 +0200

    scrolling now works

--- src/app/roleplayCharacterMap/map-display/map-display.component.ts
+++ src/app/roleplayCharacterMap/map-display/map-display.component.ts
@@ -1,3 +1,4 @@
+import { callbackify } from 'util';
 import { CharacterMap } from './../CharacterMap';
 import { CharacterMapService } from './../../character-map.service';
 import { Component, OnInit, HostListener, ApplicationRef } from '@angular/core';
@@ -13,6 +14,10 @@ export class MapDisplayComponent implements OnInit {
 
   currentCharMap: CharacterMap = new CharacterMap();
 
+  canvasContents: Array<{ x: number, y: number, image: HTMLImageElement }> = new Array<{ x: number, y: number, image: HTMLImageElement }>();
+  lastMousePosition: { x: number, y: number } = { x: 0, y: 0 };
+
+
   constructor(private characterMapService: CharacterMapService) { }
 
   ngOnInit() {
@@ -23,39 +28,78 @@ export class MapDisplayComponent implements OnInit {
   private fetchCharMap() {
     this.characterMapService.getCurrentMap().then((charMap) => {
       this.currentCharMap = charMap;
+      this.canvasContents = new Array<{ x: number, y: number, image: HTMLImageElement }>();
+      this.cleanCanvas();
+      this.addCharMapImages();
       this.updateCanvas();
     });
     this.characterMapService.getCurrentMapObserable().subscribe({
       next: (newCharMap) => {
         this.currentCharMap = newCharMap;
+        this.canvasContents = new Array<{ x: number, y: number, image: HTMLImageElement }>();
+        this.cleanCanvas();
+        this.addCharMapImages();
         this.updateCanvas();
       }
     });
   }
 
+  cleanCanvas() {
+    const context = this.canvas.getContext('2d');
+
+    context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
   updateCanvas(): void {
     const context = this.canvas.getContext('2d');
-    const image = new Image();
-    image.src = this.currentCharMap.mapImage;
-    image.onload = () => {
-      context.drawImage(image, 0, 0);
-    };
+    this.canvasContents.forEach((each) => {
+      context.drawImage(each.image, each.x, each.y);
+    });
   }
 
   @HostListener('mousemove', ['$event'])
   onClick(event) {
     if (this.mouseCurrentlyDown) {
+
+      const dx = event.x - this.lastMousePosition.x;
+      const dy = event.y - this.lastMousePosition.y;
+
+      this.lastMousePosition.x = event.x;
+      this.lastMousePosition.y = event.y;
+
+      this.canvasContents.forEach(element => {
+        element.x += dx;
+        element.y += dy;
+      });
+      this.cleanCanvas();
+      this.updateCanvas();
     }
   }
 
-  @HostListener('mousedown')
-  onMouseDown(event) {
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent) {
+    if (!this.isClickOnCanvas(event)) { return; }
     this.mouseCurrentlyDown = true;
+    this.lastMousePosition.x = event.x;
+    this.lastMousePosition.y = event.y;
+  }
+
+  isClickOnCanvas(event: MouseEvent): boolean {
+    const rect = this.canvas.getBoundingClientRect();
+    if (rect.left > event.x || rect.right < event.x) {
+      return false;
+    }
+    if (rect.bottom < event.y || rect.top > event.y) {
+      return false;
+    }
+    return true;
   }
 
   @HostListener('mouseup')
   onMouseUp(event) {
     this.mouseCurrentlyDown = false;
+    this.lastMousePosition.x = 0;
+    this.lastMousePosition.y = 0;
   }
 
   initializeCanvas() {
@@ -65,7 +109,26 @@ export class MapDisplayComponent implements OnInit {
     this.canvas.height = parent.offsetHeight;
     this.canvas.width = parent.offsetWidth;
 
-    this.updateCanvas();
+    this.addCharMapImages();
+  }
+
+  addCharMapImages(): any {
+
+    const mapBackground = new Image();
+
+    mapBackground.src = this.currentCharMap.mapImage;
+
+    this.canvasContents.push(
+      {
+        x: 0,
+        y: 0,
+        image: mapBackground
+      }
+    );
+
+    mapBackground.onload = () => {
+      this.canvas.getContext('2d').drawImage(mapBackground, 0, 0);
+    };
   }
 
 }
